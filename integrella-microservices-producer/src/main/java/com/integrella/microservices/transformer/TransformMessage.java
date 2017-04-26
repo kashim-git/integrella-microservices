83d5cd88cc80b23d0308a0f27f67b45d029bc77b
Kashim <Kashim@192.168.47.69>
2017-04-26 10:17:04 +0100

    Initial upload

---
+++ integrella-microservices-producer/src/main/java/com/integrella/microservices/transformer/TransformMessage.java
@@ -0,0 +1,33 @@
+package com.integrella.microservices.transformer;
+
+import org.apache.log4j.Logger;
+
+public class TransformMessage {
+	
+	private static Logger logger = Logger.getLogger(TransformMessage.class);
+	
+	
+	public String transform(byte[] data){
+		String message = new String (data);
+		logger.info("Original message received by transformer: " + message);
+		logger.info("New message is: " + convert(message));
+		return convert(message);
+	}
+	
+	
+	/*
+	public String transform(String data){
+		//String message = new String (data);
+		logger.info("Original message received by transformer: " + data);
+		logger.info("New message is: " + convert(data));
+		return convert(data);
+	}
+	*/
+	
+	private String convert(String message){
+		return message.toUpperCase();
+	}
+	
+	
+
+}
